f336684c17343521ac6acb12fcabad503902b5d4
Francisco Barbosa <phantomwar@gmail.com>
2024-03-18 13:55:54 -0300

    comentarios

--- dot_env.dart
+++ dot_env.dart
@@ -3,6 +3,10 @@ import 'dart:io';
 void main() {
   Map<String, String> envs = {};
   var regEx = RegExp(r'[=' '#]');
+  // Read .env file, o readAsLinesSync() retorna uma lista de linhas do arquivo
+  //o forEach() percorre cada elemento da lista
+  //o split() divide cada elemento da lista em uma string com base no argumento passado
+  // tive que verificar se o elemento da lista estava vazio antes de usar o split()
   File('.env').readAsLinesSync().forEach(
     (element) {
       if (element.isNotEmpty) {
